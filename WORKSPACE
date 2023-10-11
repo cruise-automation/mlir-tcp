f1d0d34044f156f180ebf6ffd8ca30b075e2d17e
Sambhav Jain <sambhav.jain@getcruise.com>
2023-10-10 19:43:59 -0700

    Update LICENSE to Apache v2.0 with LLVM exceptions (#4)
    As titled. Include torch-mlir dual license text. Update file headers.

--- WORKSPACE
+++ WORKSPACE
@@ -1,3 +1,8 @@
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# Also available under a BSD-style license. See LICENSE.
+
 workspace(name = "mlir_tcp_ws")
 
 load("//:deps.bzl", "third_party_deps")
--- lib/Transforms/FuseTcpOpsPass.cpp
+++ lib/Transforms/FuseTcpOpsPass.cpp
@@ -1,3 +1,12 @@
+//===------------------------------------------------------------*- C++ -*-===//
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Also available under a BSD-style license. See LICENSE.
+//
+//===----------------------------------------------------------------------===//
+
 #include "Transforms/FuseTcpOpsPass.h"
 #include "./PassDetail.h"
 
