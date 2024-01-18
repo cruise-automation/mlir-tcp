fe5bbe172d073cef633df5f0f6a14a5dd66fa9eb
Sambhav Jain <sambhav.jain@getcruise.com>
2024-01-17 17:42:50 -0800

    [Build] Switch all docker flows to run as user (non-root) (#31)
    Running docker as root has unintended side effects such as clobbering of
    user space with root-modified files (e.g. when clang-format or
    buildifier was run from within docker). This also creates permission
    issues when generating compilation database (for clangd). Another side
    benefit of this is we no longer need the patches to our GHA/CI workflows
    to update bazel cache permissions before / after the builds.

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -2,9 +2,13 @@
 
 docker build -f docker/Dockerfile \
              -t mlir-tcp:dev \
+             --build-arg GROUP=$(id -gn) \
+             --build-arg GID=$(id -g) \
+             --build-arg USER=$(id -un) \
+             --build-arg UID=$(id -u) \
              .
 
 docker run -it \
            -v "$(pwd)":"/opt/src/mlir-tcp" \
-           -v "${HOME}/.cache/bazel":"/root/.cache/bazel" \
+           -v "${HOME}/.cache/bazel":"${HOME}/.cache/bazel" \
            mlir-tcp:dev
