705aeba4b65db3a676667d87642acd910c8488f1
Sambhav Jain <sambhav.jain@getcruise.com>
2024-03-02 11:02:42 -0800

    mechanical changes / NFC

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -12,24 +12,24 @@ from lit.llvm.subst import ToolSubst
 
 # Populate Lit configuration with the minimal required metadata.
 # Some metadata is populated in lit.site.cfg.py.in.
-config.name = 'MLIR_TCP_TESTS_SUITE'
+config.name = "MLIR_TCP_TESTS_SUITE"
 config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
-config.suffixes = ['.mlir', '.py']
+config.suffixes = [".mlir", ".py"]
 
 tool_dirs = [
-  config.llvm_tools_dir,
-  config.tcp_tools_dir,
+    config.llvm_tools_dir,
+    config.tcp_tools_dir,
 ]
 
 # Make LLVM, TCP and PYTHON tools available in RUN directives
 tools = [
-  'tcp-opt',
-  'FileCheck',
-  'count',
-  'not',
-  ToolSubst('%PYTHON', config.python_executable, unresolved='ignore'),
+    "tcp-opt",
+    "FileCheck",
+    "count",
+    "not",
+    ToolSubst("%PYTHON", config.python_executable, unresolved="ignore"),
 ]
 
 llvm_config.add_tool_substitutions(tools, tool_dirs)
 
-llvm_config.with_environment('PYTHONPATH', config.python_path, append_path=True)
+llvm_config.with_environment("PYTHONPATH", config.python_path, append_path=True)
